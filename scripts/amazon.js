fcc55cf6761b394e096861da34c77cc7be243244
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-10 08:05:11 +0530

    add eventLister to add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -44,7 +44,7 @@ products.forEach((product)=>{
                     Added
                   </div>
 
-                  <button class="add-to-cart-button button-primary">
+                  <button class="add-to-cart-button button-primary js-add-to-cart">
                     Add to Cart
                   </button>
                 </div>`
@@ -52,3 +52,9 @@ products.forEach((product)=>{
 
 
 document.querySelector('.js-products-grid').innerHTML=productHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click',()=>{
+    console.log('Added Product');
+  });
+});
