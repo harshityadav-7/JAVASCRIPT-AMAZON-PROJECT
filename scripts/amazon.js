42509f4def1c404aed008654147619d784051a12
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-11 08:38:17 +0530

    use dom to get quantity set by user before addtocart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -23,7 +23,7 @@ products.forEach((product)=>{
                   </div>
 
                   <div class="product-quantity-container">
-                    <select>
+                    <select class="js-quantity-selector-${product.id}">
                       <option selected value="1">1</option>
                       <option value="2">2</option>
                       <option value="3">3</option>
@@ -58,19 +58,20 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
     const productId=button.dataset.productId;
 
+
     let matchingItem;
     cart.forEach((item)=>{
       if(productId === item.productId){
         matchingItem=item;
       }
     });
-
+    let quantity=parseInt(document.querySelector(`.js-quantity-selector-${productId}`).value);
     if(matchingItem){
-      matchingItem.quantity+=1;
+      matchingItem.quantity+=quantity;
     }else{
       cart.push({
         productId:productId,
-        quantity:1
+        quantity:quantity
       });
     }
 
