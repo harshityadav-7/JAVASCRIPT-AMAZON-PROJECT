a6310f5c7f872dbe30a04d9178aa07a3eb4b5b53
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-18 10:32:03 +0530

    add function use to formatCurrency

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 //dot dot above are used to move out of scripts sub-folder
 //module only works when we open website using live server and not directly
@@ -24,7 +25,7 @@ products.forEach((product)=>{
                   </div>
 
                   <div class="product-price">
-                    $${(product.priceCents/100).toFixed(2)}
+                    $${formatCurrency(product.priceCents)}
                   </div>
 
                   <div class="product-quantity-container">
